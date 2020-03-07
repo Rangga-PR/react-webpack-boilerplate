bcb7489649da305664f1e357159c01229b0e0f4e
rangga-pr <ranggaputraraharja@gmail.com>
2020-03-07 19:47:37 +0700

    add clean webpack plugin and core-js

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const merge = require("webpack-merge");
 const common = require("./webpack.common");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = merge(common, {
   mode: "production",
@@ -8,5 +9,6 @@ module.exports = merge(common, {
     filename: "[name]-[hash].js",
     path: path.resolve(__dirname, "dist")
   },
-  devtool: "source-map"
+  devtool: "source-map",
+  plugins: [new CleanWebpackPlugin()]
 });
