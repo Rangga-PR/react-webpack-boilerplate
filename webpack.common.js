7fbc2448e243d872ba69c7d450a254b40e614ebb
rangga-pr <ranggaputraraharja@gmail.com>
2020-03-07 19:29:07 +0700

    add file-loader to handle image assets

--- webpack.common.js
+++ webpack.common.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 module.exports = {
   entry: {
     app: "./src/index.js"
@@ -8,6 +10,16 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         loader: "babel-loader"
+      },
+      {
+        test: /\.(svg|ico|png|jpg|jpeg|gif|webp)$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: "[name]-hash.[ext]",
+            outputPath: path.resolve(__dirname, "dist/assets/img")
+          }
+        }
       }
     ]
   }
